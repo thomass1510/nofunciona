940fcdd3d0be56b43add1c70a9ae5d0d50eef16b
thomas.solis.1510 <thomasgsolis@hotmail.com>
2022-01-31 22:08:56 -0300

    adding navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+
+const NavBar = ()=>{
+    return (
+    <nav className='menu'>
+        <label className='logo'>GameCase</label>
+        <ul className='menuItems'>
+            <li>Consolas</li>
+            <li>VideoJuegos</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+    )
+}
+
+export default NavBar
